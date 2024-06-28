59b49697876674615f80af740017ea26b7864dac
masatosotoma <masatosotoma@gmail.com>
2024-06-27 22:03:33 -0400

    Created box elements

--- search-tutorial/src/App.js
+++ search-tutorial/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <h2>Searching App</h2>
+        <input type="text" />
+        <div className="content">
+          <div className="box">
+            <h3>User Name</h3>
+            <hr />
+            <p>Email Address</p>
+          </div>
+          <div className="box">
+            <h3>User Name</h3>
+            <hr />
+            <p>Email Address</p>
+          </div>
+          <div className="box">
+            <h3>User Name</h3>
+            <hr />
+            <p>Email Address</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
