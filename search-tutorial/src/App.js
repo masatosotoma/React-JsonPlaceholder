bf15cd49b8b797047df88cc6dff92016ef80fc07
masatosotoma <masatosotoma@gmail.com>
2024-06-28 22:20:33 -0400

    Updated the fetch API address

--- search-tutorial/src/App.js
+++ search-tutorial/src/App.js
@@ -1,27 +1,32 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => setUsers(data));
+  }, []);
+
+  console.log(users);
+
   return (
     <div className="App">
       <div className="main">
         <h2>Searching App</h2>
         <input type="text" />
         <div className="content">
-          <div className="box">
-            <h3>User Name</h3>
-            <hr />
-            <p>Email Address</p>
-          </div>
-          <div className="box">
-            <h3>User Name</h3>
-            <hr />
-            <p>Email Address</p>
-          </div>
-          <div className="box">
-            <h3>User Name</h3>
-            <hr />
-            <p>Email Address</p>
-          </div>
+          {users.map((user) => (
+            <div className="box">
+              <h3>{user.name}</h3>
+              <hr />
+              <p>{user.email}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
